979c884458c541d81ccce352684c521a6a31dd15
shanti poudyel <santipoudyal@gmail.com>
2020-10-12 11:28:09 -0400

    file added dice

--- main.js
+++ main.js
@@ -1,36 +1,36 @@
-let roll = document.querySelector("#roll");
-let totalResult = document.querySelector("#result");
-let showRolls = document.querySelector("#showRoll");
+let roll = document.querySelector(".Roll");
+let totalresult = document.querySelector("#total");
+let showtherolls = document.querySelector(".showtherolls");
 let lists = document.querySelector("#lists");
-roll.addEventListener("click", function () {
-  let dieRolls = [];
-  let userInput = document.querySelector("#num").value;
-  let num = 0;
-  let total = 0;
-  while (num < userInput) {
-    let x = Math.floor(Math.random() * 6) + 1;
-    dieRolls.push(x);
-    total += dieRolls[num];
-    num += 1;
-  }
-  totalResult.innerHTML = total;
-  console.log(dieRolls);
-  showRolls.addEventListener("click", function () {
-    str = "<ol>";
-    dieRolls.forEach(function (die) {
-      str += "<li>" + die + "</li>";
-    });
-    str += "</ol>";
-    lists.innerHTML = str;
-  });
-});
-let numberofresets = document.querySelector("#resets-butttom");
-let resetbutton = document.querySelector("#num-resets"); 
-  numberofresets.addEventListener("clicked", function() {
-    let numberofresets = Number(resetbutton.innerHTML) + 1;
-    resetbutton.innerHTML = numberofresets;
-dieRolls.innerHTML =0
+let numofdice = document.querySelector("#number-of-dice");
+let numberofresets = document.querySelector("#reset-button");
+let dierolls = [];
+let counter = 0;
+roll.addEventListener("click", function(){
+let dicenum = numofdice.value 
+let total = 0;
+while (counter<dicenum) {
+    let x = Math.floor(Math.random()*6 +1)
+    dierolls.push(x)
+    total +=x
+    counter++
+    
+}
+totalresult.innerHTML=total
 })
 
-
-
+showtherolls.addEventListener("click", function(){
+    str = "<ol>";
+    dierolls.forEach(function (dicenum){
+        str += "<li>" + dicenum + "</li>";
+    });
+    str +="</ol>";
+    lists.innerHTML=str
+});
+numberofresets.addEventListener("click", function() {
+    
+    totalresult.innerHTML = 0;
+    numofdice.value =0;
+    lists.innerHTML = '';
+    
+})
